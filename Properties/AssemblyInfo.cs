7dfc866933571ba56296728f24f862f888566f64
Reelix <reelix+github@gmail.com>
2019-12-13 07:14:00 +0200

    - AssemblyInfo.cs - Finally updated - Reecon.csproj - Removed an unused reference

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReeHack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Reecon")]
+[assembly: AssemblyDescription("A small program for network recon")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReeHack")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Reelix")]
+[assembly: AssemblyProduct("Reecon")]
+[assembly: AssemblyCopyright("N/A - Just say thanks :)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.8.0")]
+[assembly: AssemblyFileVersion("0.0.8.0")]
