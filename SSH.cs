20c285a86d88b79550d99fbc97c0071d591c926d
Reelix <reelix@gmail.com>
2020-07-16 14:48:25 +0200

    Revert "Revert Me!"
    This reverts commit 6df4a70b5eb459d8f9bb7c1255137f8a97f5d410.

--- SSH.cs
+++ SSH.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -25,7 +26,6 @@ namespace Reecon
                 using (Socket sshSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
                 {
                     sshSocket.Connect(ip, port);
-                    // s
                     int bytes = sshSocket.Receive(buffer, buffer.Length, 0);
                     string versionMessage = Encoding.ASCII.GetString(buffer, 0, bytes);
                     versionMessage = versionMessage.Trim().Replace(Environment.NewLine, "");
