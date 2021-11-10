0271bfacdf7a20a72af7a51fee6e8a428cc8be48
Reelix <reelix+github@gmail.com>
2021-11-11 01:03:25 +0200

    Better Linux setup

--- setup-dotnet.sh
+++ setup-dotnet.sh
@@ -1,5 +1,6 @@
-wget https://download.visualstudio.microsoft.com/download/pr/0ce1c34f-0d9e-4d9b-964e-da676c8e605a/7a6c353b36477fa84f85b2821f2350c2/dotnet-runtime-6.0.0-linux-x64.tar.gz
-DOTNET_FILE=dotnet-runtime-6.0.0-linux-x64.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/0ce1c34f-0d9e-4d9b-964e-da676c8e605a/7a6c353b36477fa84f85b2821f2350c2/dotnet-runtime-6.0.0-linux-x64.tar.gz -O dotnet-setup.tar.gz
+DOTNET_SETUP_FILE=dotnet-setup.tar.gz
 export DOTNET_ROOT=~/dotnet
-mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
+mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_SETUP_FILE" -C "$DOTNET_ROOT"
 export PATH=$PATH:$DOTNET_ROOT
+rm "$DOTNET_SETUP_FILE"
